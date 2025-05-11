116f246aaee3ae6f27a1ca30cb28ebeb29bd9483
Matthew 🪷 <matthew@urgero.net>
2025-05-10 23:01:17 -0500

    Add links for Summer Solstice 2025 to `/goods` page (#7)

--- app/util/types.ts
+++ app/util/types.ts
@@ -1,5 +1,11 @@
-import type { PageLink } from "@nuxt/ui-pro/types";
+import type { PageLink } from '@nuxt/ui-pro/types'
 
 export type GoodsPageLink = PageLink & {
   version?: string
+}
+
+export type GoodsPage = {
+  title: string
+  description: string
+  links: GoodsPageLink[]
 }
